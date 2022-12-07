f444c96181437979af7dd750dc8df1702aa05730
Olivier Duhart <olivier.duhart@sage.com>
2022-12-07 11:26:26 +0100

    fix todos id

--- Todo.cs
+++ Todo.cs
@@ -3,7 +3,7 @@ namespace LiveBlazorWasm.Data
 {
     public class TodoItem
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         
         public string Description { get; set; }
         
@@ -11,7 +11,7 @@ namespace LiveBlazorWasm.Data
 
         public override string ToString()
         {
-            return $"{Id ?? "null"} - {Description} : {(Done ? "done" : "not done")}";
+            return $"{Id} - {Description} : {(Done ? "done" : "not done")}";
         }
     }
 }
